cd5ab76efea4e2ee406d298e94df0170e99d4402
Your Name <you@example.com>
2021-08-23 16:46:46 +0800

    16点46分

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion: This is descripttion
+ * @version: 0.0.1
+ * @Author: liuhb
+ * @Date: 2021-08-23 13:47:14
+ * @LastEditors: liuhb
+ * @LastEditTime: 2021-08-23 16:46:28
+ */
 const path = require("path");
 module.exports = {
   mode: "production",
@@ -29,41 +37,3 @@ module.exports = {
     ],
   },
 };
-
-// const path = require("path");
-// module.exports = {
-//   mode: "production",
-//   entry: "./src/index.js",
-//   // devtool: 'inline-source-map',
-//   devServer: {
-//     contentBase: "./dist",
-//     compress: true,
-//     open: false,
-//   },
-//   output: {
-//     publicPath: "/",
-//     // 指定打包后输出的文件名
-//     filename: "index.js",
-//     // 指定打包后输出的目录
-//     path: path.resolve(__dirname, "dist"),
-//   },
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "src"),
-//     },
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: "babel-loader",
-//           options: {
-//             presets: [["@babel/preset-env"]],
-//           },
-//         },
-//       },
-//     ],
-//   },
-// };
